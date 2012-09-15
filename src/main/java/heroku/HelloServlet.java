ea95cde45eaadca57eb3dd43e3eae2351c5322f9
Edson Yanaga <edson@yanaga.com.br>
2012-09-15 14:07:36 -0300

    Criado HelloServlet.

--- src/main/java/heroku/HelloServlet.java
+++ src/main/java/heroku/HelloServlet.java
@@ -1,5 +1,18 @@
 package heroku;
 
-public class HelloServlet {
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter pw = resp.getWriter();
+        pw.println("<html><body><h1>Hello</h1></body></html>");
+    }
     
 }
